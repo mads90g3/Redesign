aff0d3b4c5d6c43f9ac251a6e64025fe8ca3f4fb
Theresa Aagaard Hansen <theresa_aagaard@Theresas-MacBook-Air.local>
2020-12-15 21:57:58 +0100

    flyttet logo uden for nav, fået burgermenu til at virke more or less

--- script.js
+++ script.js
@@ -2,22 +2,18 @@ window.addEventListener("load", sidenVises);
 
 function sidenVises() {
     console.log("sidenVises");
-    document.querySelector("#menuknap").classList.remove("hidden");
-    document.querySelector("#menuknap").addEventListener("click", visMenu);
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 }
 
-function visMenu() {
-    console.log("visMenu");
-    document.querySelector("#menu").classList.remove("hidden");
-    document.querySelector("#exit_knap").classList.remove("hidden");
-    document.querySelector("#menuknap").classList.add("hidden");
-    document.querySelector("#exit_knap").addEventListener("click", skjulMenu);
-}
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
 
-function skjulMenu() {
-    console.log("skjulMenu");
-    document.querySelector("#menu").classList.add("hidden");
-    document.querySelector("#exit_knap").classList.add("hidden");
-    document.querySelector("#menuknap").classList.remove("hidden");
-    document.querySelector("#menuknap").addEventListener("click", visMenu);
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent = "☰";
+    } else {
+        document.querySelector("#menuknap").textContent = "X";
+    }
 }
